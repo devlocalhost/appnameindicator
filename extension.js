655b7c83a61dcbcd042948613c84938626a4a525
devlocalhost <lazarecobani@gmail.com>
2025-10-11 00:14:38 +0300

    let user enable/disable app icon/label

--- extension.js
+++ extension.js
@@ -14,14 +14,17 @@ export default class AppNameIndicator extends Extension {
             x_align: Clutter.ActorAlign.START,
             style_class: 'panel-button'
         })
+        
         this.icon = new St.Icon({ style_class: 'system-status-icon' })
         this.label = new St.Label()
         this.box.add_child(this.icon)
         this.box.add_child(this.label)
+        
         Main.panel._leftBox.insert_child_at_index(this.box, 1)
 
         this.winSignal = global.display.connect('notify::focus-window', () => this.update())
-        this.settSignal = this.settings.connect('changed::format', () => this.update())
+        this.settSignal = this.settings.connect('changed', () => this.update())
+        
         this.update()
     }
 
@@ -29,6 +32,7 @@ export default class AppNameIndicator extends Extension {
         if (this.winSignal) global.display.disconnect(this.winSignal)
         if (this.settSignal) this.settings.disconnect(this.settSignal)
         if (this.box) Main.panel._leftBox.remove_child(this.box)
+        
         this.box = this.icon = this.label = null
         this.settings = null
     }
@@ -36,14 +40,16 @@ export default class AppNameIndicator extends Extension {
     update() {
         const w = global.display.focus_window
         if (!w) { this.box.visible = false; return }
+        
         this.box.visible = true
 
-        // Resolve app info
         const app = Shell.WindowTracker.get_default().get_window_app(w)
         let name = '', gicon = null
+        
         if (app) {
             const id = app.get_id()
             const info = Gio.DesktopAppInfo.new(id)
+            
             if (info) {
                 name = info.get_display_name() || info.get_name() || ''
                 gicon = info.get_icon()
@@ -54,20 +60,26 @@ export default class AppNameIndicator extends Extension {
         } else {
             name = w.get_wm_class() || ''
         }
-
-        // Fallbacks
+        
         if (!name) name = w.get_title() || ''
 
         const fmt = this.settings.get_string('format')
         const cls = w.get_wm_class() || ''
         const title = w.get_title() || ''
-        this.label.text = fmt
-            .replace('{class}', cls)
-            .replace('{title}', title)
-            .replace('{name}', name)
 
-        this.icon.visible = !!gicon
-        if (gicon) this.icon.gicon = gicon
+        const showIcon = this.settings.get_boolean('show-icon')
+        const showText = this.settings.get_boolean('show-text')
+
+        this.icon.visible = showIcon && !!gicon
+        if (showIcon && gicon) this.icon.gicon = gicon
+
+        if (showText) {
+            this.label.text = fmt
+                .replace('{class}', cls)
+                .replace('{title}', title)
+                .replace('{name}', name)
+        } else this.label.text = ''
+
+        this.box.visible = showIcon || showText
     }
 }
-
--- prefs.js
+++ prefs.js
@@ -1,22 +1,67 @@
-import Adw from 'gi://Adw'
-import Gtk from 'gi://Gtk'
 import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
+import Adw from 'gi://Adw'
+import Gio from 'gi://Gio'
+import Gtk from 'gi://Gtk'
 
 export default class AppNamePrefs extends ExtensionPreferences {
     fillPreferencesWindow(window) {
         const settings = this.getSettings()
-        const page = new Adw.PreferencesPage()
-        const group = new Adw.PreferencesGroup({ title: 'Format' })
 
-        const row = new Adw.EntryRow({
-            title: 'Display format',
+        const page = new Adw.PreferencesPage()
+        const group = new Adw.PreferencesGroup({
+            title: 'Window Label Format',
+            description: 'Customize how the active app is displayed on the panel.',
+        })
+        page.add(group)
+
+        const formatRow = new Adw.ActionRow({ title: 'Format string' })
+        const entry = new Gtk.Entry({
             text: settings.get_string('format'),
+            placeholder_text: '{name} - {title}',
+            hexpand: true,
+            valign: Gtk.Align.CENTER,
+            halign: Gtk.Align.END,
         })
         
-        row.connect('changed', () => settings.set_string('format', row.text))
+        entry.connect('changed', () =>
+            settings.set_string('format', entry.get_text())
+        )
         
-        group.add(row)
-        page.add(group)
+        formatRow.add_suffix(entry)
+        formatRow.activatable_widget = entry
+
+        const infoRow = new Adw.ActionRow({
+            title: 'Placeholders',
+            subtitle:
+                '{name}: App name (Extensions)\n' +
+                '{class}: Application class (org.gnome.Shell.Extensions)\n' +
+                '{title}: Window title (App Name Indicator)',
+            activatable: false,
+        })
+        
+        group.add(formatRow)
+        group.add(infoRow)
+
+        const iconRow = new Adw.ActionRow({ title: 'Show icon' })
+        const iconSwitch = new Gtk.Switch({
+            active: settings.get_boolean('show-icon'),
+            valign: Gtk.Align.CENTER,
+        })
+        settings.bind('show-icon', iconSwitch, 'active', Gio.SettingsBindFlags.DEFAULT)
+        iconRow.add_suffix(iconSwitch)
+        iconRow.activatable_widget = iconSwitch
+        group.add(iconRow)
+
+        const textRow = new Adw.ActionRow({ title: 'Show text' })
+        const textSwitch = new Gtk.Switch({
+            active: settings.get_boolean('show-text'),
+            valign: Gtk.Align.CENTER,
+        })
+        settings.bind('show-text', textSwitch, 'active', Gio.SettingsBindFlags.DEFAULT)
+        textRow.add_suffix(textSwitch)
+        textRow.activatable_widget = textSwitch
+        group.add(textRow)
+
         window.add(page)
     }
 }
